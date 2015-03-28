e870655cf25d1c099120436ece791cee9f925942
Anders Antila <anders.antila@gmail.com>
2015-03-28 23:35:45 +0100

    Fixed coinhunter

--- src/js/game/main.js
+++ src/js/game/main.js
@@ -4,7 +4,7 @@ window.onload = function () {
   var game
     , ns = window['phaser'];
 
-  game = new Phaser.Game(640, 480, Phaser.AUTO, 'phaser-game');
+  game = new Phaser.Game(1920, 1080, Phaser.AUTO, 'phaser-game');
   game.state.add('boot', ns.Boot);
   game.state.add('preloader', ns.Preloader);
   game.state.add('menu', ns.Menu);

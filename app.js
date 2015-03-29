daac605c890df4d3e118c08b4aca75c1b6383cc1
Anders Antila <anders.antila@gmail.com>
2015-03-29 13:01:53 +0200

    More cleanup and now detects IP.

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-
 /**
  * Module dependencies.
  */
-
 var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var fs = require('fs');
+var os = require('os');
 
-console.log(server.address());
+var ip;
+
+// Get IP and interface
+// http://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
+var ifaces = os.networkInterfaces();
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log('Running server on interface ' + ifname + ':' + alias + ', IP:', iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log('Running server on interface ' + ifname + ', IP:', iface.address);
+    }
+    ip = iface.address;
+  });
+});
+
+// Write IP to file, for config
+fs.writeFile("./src/js/address.js", 'var server = "' + ip + '";'); 
 
 server.listen(80);
 
@@ -23,11 +49,9 @@ var votes = {};
 
 io.on('connection', function (socket) {
   socket.on('disconnect', function() {
-    console.log('Got disconnect!');
-
       users.forEach(function(user, i) {
         if (user.userId === socket.userId) {
-          console.log('removing', user);
+          console.log(user.name, 'disconnected');
           users.splice(i--, 1);
         }
       });
@@ -35,8 +59,6 @@ io.on('connection', function (socket) {
       socket.broadcast.emit('users', users);
       socket.emit('users', users);
   });
-
-  console.log('connected', socket.id);
 
   socket.emit('users', users);
   socket.broadcast.emit('users', users);
@@ -75,24 +97,21 @@ io.on('connection', function (socket) {
     }
 
     socket.broadcast.emit('votes', votes );
-    // socket.emit('votes', votes );
   });
 
   socket.on('setName', function (data) {
-    console.log('setName', data);
       users.forEach(function(user, i) {
         if (user.userId === socket.userId) {
           users[i].name = data.name;
         }
       });
+      console.log(data.name, 'joined');
 
       socket.emit('users', users);
       socket.broadcast.emit('users', users);
   });
 
   socket.on('setId', function (userId) {
-    console.log('setId', userId);
-
     var foundUser = false;
       users.forEach(function(user, i) {
         if (user.userId === socket.userId) {
@@ -100,12 +119,11 @@ io.on('connection', function (socket) {
         }
       });
     if (foundUser === false) {
-      console.log('adding');
       socket.userId = userId
       users.push({userId: userId});
     }
 
-      socket.emit('users', users);
-      socket.broadcast.emit('users', users);
+    socket.emit('users', users);
+    socket.broadcast.emit('users', users);
   });
 });
